ba640a9078c7307bd742dd9e4ce5a8f083a9b70c
Adrian Perez de Castro <aperez@igalia.com>
2017-01-06 01:03:25 +0200

    Refactor filtering of PurgeInfo items without valid event_id
    This saves checking later on for valid event_id in all iterations of two
    loops.

--- synpurge/cli.py
+++ synpurge/cli.py
@@ -123,20 +123,18 @@ def purge(path: "configuration file",
                                        now - purge.config.keep,
                                        purge.config.token)
 
+    # Remove the rooms for which a suitable reference event couldn't be found.
+    purges = [p for p in purges if p.event_id is not None]
+    num_purges = len(purges)
+
     if pretend:
         for purge in purges:
-            if purge.event_id is None:
-                continue
             print("{} {} ({}, keep up to {})".format(purge.room_id, purge.event_id,
                                                      purge.config.name,
                                                      (now - purge.config.keep).humanize()))
         return
 
     for current, purge in zip(itertools.count(1), purges):
-        if purge.event_id is None:
-            log.info("Skipped (%i/%i) for room %s (%s), event %s",
-                     current, num_purges, purge.room_id, purge.config.name, purge.event_id)
-            continue
         log.info("Purging (%i/%i) for room %s (%s), event %s",
                  current, num_purges, purge.room_id, purge.config.name, purge.event_id)
         try:
